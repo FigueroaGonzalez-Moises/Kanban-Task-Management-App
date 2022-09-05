1896ab0d955e1d742a3cbe9694626b0814c74778
username <yashiroisanagi@gmail.com>
2022-09-05 15:10:54 -0500

    Added css grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+import Sidenav from "components/Sidenav";
+import "scss/App.scss";
+
+const App = () => {
+	return (
+		<div className="App">
+			<div className="pageWrapper">
+				<Sidenav />
+			</div>
+		</div>
+	);
+};
 
 export default App;
