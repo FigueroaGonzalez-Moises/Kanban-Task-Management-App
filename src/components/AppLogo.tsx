3b70d9a174850bc9cdbc1f6fad4cf7b8294da4ed
username <yashiroisanagi@gmail.com>
2022-09-06 11:00:29 -0500

    🚀 Installed fontawesome; Added App Icon

---
+++ src/components/AppLogo.tsx
@@ -0,0 +1,8 @@
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const AppLogo = () => {
+	return <FontAwesomeIcon icon={faPaperPlane} />;
+};
+
+export default AppLogo;
