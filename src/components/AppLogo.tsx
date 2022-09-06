da078ed56fbeafcce88c5fdcc75ba71f7e20fa17
username <yashiroisanagi@gmail.com>
2022-09-06 12:10:46 -0500

    🎨🎉 CSS init/changes :V

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -2,7 +2,7 @@ import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AppLogo = () => {
-	return <FontAwesomeIcon icon={faPaperPlane} />;
+	return <FontAwesomeIcon icon={faPaperPlane} className="purple" />;
 };
 
 export default AppLogo;
