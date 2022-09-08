e5a2e9cc4ba1811888700c4293eb7b09eaa0e9b6
username <yashiroisanagi@gmail.com>
2022-09-08 18:34:04 -0500

    📦🎨 Sidenav theme toggler :V

---
+++ src/components/Icons/MoonIcon.tsx
@@ -0,0 +1,8 @@
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const MoonIcon = () => {
+	return <FontAwesomeIcon icon={faMoon} />;
+};
+
+export default MoonIcon;
---
+++ src/components/Icons/SunIcon.tsx
@@ -0,0 +1,8 @@
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SunIcon = () => {
+	return <FontAwesomeIcon icon={faSun} />;
+};
+
+export default SunIcon;
